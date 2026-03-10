69751ff0d7ac7630fb4a7d84e845e9ce763bddcd
Freelancer <freelance@test.com>
2026-03-10 21:32:41 +0700

    Sắp xếp Post

--- scripts/buildBlogIndex.js
+++ scripts/buildBlogIndex.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const matter = require("gray-matter");
+
 const blogDir = path.join(__dirname, "../public/content/blog");
 const outputFile = path.join(blogDir, "index.json");
 
@@ -9,7 +11,17 @@ try {
 
   const posts = files
     .filter((file) => file.endsWith(".md"))
-    .map((file) => file.replace(".md", ""));
+    .map((file) => {
+      const filePath = path.join(blogDir, file);
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+      const { data } = matter(fileContent);
+      return {
+        slug: file.replace(".md", ""),
+        date: data.date ? new Date(data.date).getTime() : 0,
+      };
+    })
+    .sort((a, b) => b.date - a.date)
+    .map((post) => post.slug);
 
   fs.writeFileSync(outputFile, JSON.stringify(posts, null, 2));
 
